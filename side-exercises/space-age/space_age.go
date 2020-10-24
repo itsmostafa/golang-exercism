75094962afd28408a6b2be90b703f928c8f1bde9
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-10-24 13:23:21 -0500

    side exercise: space age

---
+++ side-exercises/space-age/space_age.go
@@ -0,0 +1,23 @@
+package space
+
+// Planet ...
+type Planet string
+
+// Age ...
+func Age(seconds float64, planet Planet) float64 {
+	var earthYear float64 = 31557600
+	earthAge := seconds / earthYear
+
+	orbitalPeriod := map[Planet]float64{
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Earth":   1.0,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
+	}
+
+	return earthAge / orbitalPeriod[planet]
+}
